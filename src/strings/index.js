763ee40cb28bf68a313f5dfebc5c7e3cb1840007
ctamez17 <ctamez17@gmail.com>
2020-07-12 19:31:48 -0500

    all tests pass

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,33 @@
+const { strings } = require("..")
+
 const split = (str, delim) => {
   // write code for strings.split
-
+  delim = str.split('-')
+  return delim;
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let subStr = "";
+  let arrOfPairs = [];
+  for (let i = 0; i < str.length; i+=2){
+    subStr = str[i] + str[i+1];
+    arrOfPairs.push(subStr);
+  }
+  return arrOfPairs;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let newStr = "";
+  let c = "";
+  let strLength = str.length;
+  for (let i = 0; i < strLength; i++) {
+    c = str[str.length-1];
+    str = str.slice(0, str.length-1);
+    newStr += c;
+  }
+  return newStr;
 }
 
 module.exports = {
